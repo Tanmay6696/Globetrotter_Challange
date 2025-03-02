5b475d598851fd23e8936122fffa2e54308b9b3a
Tanmay6696 <99316889+Tanmay6696@users.noreply.github.com>
2025-03-02 08:04:53 +0530

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,39 +1,10 @@
 import dotenv from "dotenv";
-import {app} from './app.js'
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
-import { createServer } from "http";
-import { Server } from "socket.io";
 
-dotenv.config({path:"./env"});
+dotenv.config({ path: "./env" });
 
-const server = createServer(app); 
-const connectedUsers = {};
-const io = new Server(server, 
-    { 
-        cors: { 
-            origin: process.env.CLIENT_URL || "*"
-        } 
-    }
-);
-io.on("connection", (socket) => {
-  
-    socket.on("register", (userId) => {
-        console.log(`User ${userId} joined room`);
+await connectDB(); // Ensure database is connected before handling requests
 
-      connectedUsers[userId] = socket.id;
-    });
-  
-    
-  
-    socket.on("disconnect", () => {
-      console.log("User disconnected:", socket.id);
-      Object.keys(connectedUsers).forEach((key) => {
-        if (connectedUsers[key] === socket.id) delete connectedUsers[key];
-      });
-    });
-  });
-export {io};
-export default async function handler(req, res) {
-  await connectDB();
-  return app(req, res);
-}
+// Required for Vercel Deployment
+export default app;
